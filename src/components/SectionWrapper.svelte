d16aa975ee988f7dd923babee86e4f94c5de1c54
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2025-06-16 12:41:53 +0300

    refine card

--- src/components/SectionWrapper.svelte
+++ src/components/SectionWrapper.svelte
@@ -1,8 +1,8 @@
 <script>
     let { children, id } = $props();
 </script>
-<section {id} class={"min-h-screen flex flex-col px-4" }>
-    <div class="flex flex-col flex-1 max-w-[400px] mx-auto w-full">
+<section {id} class={"min-h-screen flex flex-col px-4 "}>
+    <div class="flex flex-col flex-1 max-w-[1400px] mx-auto w-full">
         {@render children?.()}
     </div>
 </section>
