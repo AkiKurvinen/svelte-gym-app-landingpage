86c1ccf310454dc8f44195241b6f5896b0358b8d
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2025-06-23 12:07:37 +0300

    fix typo nav

--- src/components/Organisms/Header.svelte
+++ src/components/Organisms/Header.svelte
@@ -1,10 +1,10 @@
 <script>
-    import Nav from "./NavConstroller.svelte";
+    import NavController from "./NavController.svelte";
     import Logo from "../Atoms/Logo.svelte";
     export let fixed = false;
 </script>
 
 <header class={`flex justify-between border-b border-solid border-black items-center relative px-2 z-20 h-20 ${fixed ? 'fixed top-0 left-0 w-full' : ''}`}>
     <a href="/"><Logo/></a>
-    <Nav/>
+    <NavController/>
 </header>
