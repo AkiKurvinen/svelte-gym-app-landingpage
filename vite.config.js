d8a4c304a1690a6b34ceea7cdb6cd6ab239771b9
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2025-06-24 11:32:25 +0300

    test vite build

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  base: '/REPO/',
   plugins: [tailwindcss(), sveltekit()],
   test: {
     projects: [{
